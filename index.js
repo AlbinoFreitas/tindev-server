10f0bff01e740dfcdf9a5b941725bfc5c0447668
AlbinoFreitas <albino_freitas@hotmail.com>
2019-08-11 22:10:14 -0300

    Enable access from same origin

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./src/routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
